380c9d2788819d7df63dd5de44e228b35c7fb78e
Solis-Chavez <rsolis12@cps.k12.il.us>
2024-04-30 11:56:39 -0500

    more conditonals

---
+++ day3.py
@@ -0,0 +1,34 @@
+ #booleans
+print(10 > 9) # is this true or false?
+#booleans are used to evalute expressions and conditions in python 
+# they are used to check if an experssion is true or false
+# broughtFood = True
+# print(broughtFood)
+# is_raining = True
+# if is_raining:
+#     print("Take an unbrella!")
+# else:
+#     print("No umbrella needed.")
+
+# tempurature = 15
+# if tempurature > 30: 
+#         print(" It's warm")
+#         print("Drink water")
+# elif tempurature > 20:
+#         print("It's nice")
+# else:
+#         print("It's cold")
+# print("Done")
+
+age = 12
+# if age >= 18:
+#         message = "Eligible"
+# else:
+#         message = "Not eligible"
+
+# message = "Eligible" if age >= 19 else "Not eligible" # equivalant to the 4 codes above
+# print(message)
+# code above called ternary operator
+
+#Logical Operators 
+ 
