ad009e51460699e19df0a69bad39dd455e306e39
Solis-Chavez <rsolis12@cps.k12.il.us>
2024-05-06 12:14:43 -0500

    day_2learning

--- conditionalsReview2.py
+++ conditionalsReview2.py
@@ -4,30 +4,46 @@
 
 #functions allow me to wrap up the code and reuse it
  #fucntions are defined with the def keyword
-def scoreMe():
-    score = int(input("Enter your socre:"))
-    print("You entered", score)
+# def scoreMe():
+#     score = int(input("Enter your socre:"))
+#     print("You entered", score)
     #Implement the logic to determine the grade based on the score:
-    # A score of 90 and above is an "A".
-    if score >= 90:
-        print("A") 
-    # A score of 80 to 89 is a "B".
-    elif score >=80 and score <90:
-        print("B")
-    # A score of 70 to 79 is a "C".
-    elif score >=70 and score <80:
-        print("D")
-    # A score of 60 to 69 is a "D".
-    elif score >=60 and score <70:
-        print("D")
-    # A score below 60 is an "F".
-    elif score <60:
-        print("F")
+    # # A score of 90 and above is an "A".
+    # if score >= 90:
+    #     print("A") 
+    # # A score of 80 to 89 is a "B".
+    # elif score >=80 and score <90:
+    #     print("B")
+    # # A score of 70 to 79 is a "C".
+    # elif score >=70 and score <80:
+    #     print("D")
+    # # A score of 60 to 69 is a "D".
+    # elif score >=60 and score <70:
+    #     print("D")
+    # # A score below 60 is an "F".
+    # elif score <60:
+    #     print("F")
     # Ensure the score entered is within the valid range (0-100). If not, print an error message.
 
 #call the function
-scoreMe()
-scoreMe()
-scoreMe()
+# scoreMe()
+# scoreMe()
+# scoreMe()
+
+dietary_restrictions = input("Do you have any dietary restrictions?")
+if dietary_restrictions == "yes": 
+    type = (input("What kind?"))
+else:
+    print("Thank you")
+
+if type == "Vegetarian":
+    print("You can eat a salad.")
+elif type == "Gluten-free":
+    print("You can eat gluten-free pasta.")
+elif type =="nut free":
+    print("You can eat a burger")
+
+
+
 
 
---
+++ whileLoops.py
@@ -0,0 +1,37 @@
+
+nums = [1, 2, 3, 4, 5]
+
+# for num in nums:
+#     if num == 3:
+#         print("Found!")
+#         continue
+#     print(num)
+
+#nested for loops
+# for num in nums:
+#     for letter in "abs":
+#         print(num,letter)
+
+#ranges
+#first number is called inlsuive
+#last number is the exclusive
+# for i in range(1, 28):
+#     print(i)
+
+#while loops
+x = 0
+
+while True:
+    # if x == 5:
+    #     break
+    print(x)
+    x += 1
+
+# while True: 
+#     print("Enter your name:")
+#     name = input()
+#     if name == 'Ruby':
+#         print("your name is " + name)
+#         break
+#     else: 
+#         print("Thank you")
